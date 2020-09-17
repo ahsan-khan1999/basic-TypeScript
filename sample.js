667b4c9ab0c80048d6a7b9356d89f1c5da99eacf
ahsan-khan1999 <ahsan.ak943@gmail.com>
2020-09-17 17:33:54 +0500

    basic type checking in typeScript

---
+++ sample.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+var a = 10;
+a = 12;
+var firstName = 'mark';
+console.log(firstName);
+var names = ['roman', 'dean', 'seth'];
+console.log(names);
+var ob = {
+    name: string, 'ashsan': ,
+    age: 15,
+    sex: 'male'
+};
+console.log(ob.name);
---
+++ sample.ts
@@ -0,0 +1,19 @@
+console.log('Hello World')
+
+let a:number = 10;
+a = 12
+
+let firstName:string = 'mark';
+console.log(firstName)
+
+
+let names: string[] = ['roman','dean','seth']
+console.log(names);
+
+let ob ={
+    name:'ashsan',
+    age:15,
+    sex:'male',
+}
+
+console.log(ob.name)
